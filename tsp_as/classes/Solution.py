178e709ac9a1188fb5369fb52bfc92048aaaeada
l-lan <l.lan@vu.nl>
2022-10-12 08:35:06 +0200

    Cachce distance and idle wait costs

--- tsp_as/classes/Solution.py
+++ tsp_as/classes/Solution.py
@@ -19,6 +19,9 @@ class Solution(State):
         self.unassigned = unassigned if unassigned is not None else []
         self.params = params
 
+        self._distance_cost = None
+        self._idle_wait_cost = None
+
     def __deepcopy__(self, memodict={}):
         self.params.trajectory.append(self.tour)
         return Solution(self.params, copy(self.tour), copy(self.unassigned))
@@ -32,18 +35,25 @@ class Solution(State):
     def __eq__(self, other):
         return self.tour == other.tour
 
+    @property
+    def distance_cost(self):
+        return self._distance_cost
+
+    @property
+    def idle_wait_cost(self):
+        return self._idle_wait_cost
+
     @staticmethod
     def compute_distance(tour, params):
         """
         Compute the distance of the tour.
         """
-        # TODO double check if this is correct
         to = [0] + tour
         fr = tour + [0]
         return params.distances[to, fr].sum()
 
     @staticmethod
-    def compute_objective(tour, params):
+    def compute_idle_wait(tour, params):
         if params.objective == "htp":
             return heavy_traffic_pure(tour, params)[1]
         elif params.objective == "hto":
@@ -52,11 +62,17 @@ class Solution(State):
             return true_optimal(tour, params)[1]
 
     def objective(self):
-        dist = self.compute_distance(self.tour, self.params)
-        idle_wait = self.compute_objective(self.tour, self.params)
+        """
+        Return the objective value. This is a weighted sum of the distance
+        and the idle and waiting times.
+        """
+        # TODO Need to add omega weights here?
+        return self.distance_cost + self.idle_wait_cost
+        # dist = self.compute_distance(self.tour, self.params)
+        # idle_wait = self.compute_idle_wait(self.tour, self.params)
 
-        # TODO Need to add weights here?
-        return dist + idle_wait
+        # # TODO Need to add omega weights here?
+        # return dist + idle_wait
 
     def insert_cost(self, idx: int, customer: int) -> float:
         """
@@ -76,16 +92,29 @@ class Solution(State):
         dist = self.params.distances[pred, succ]
         cand.insert(idx, customer)
 
-        return dist + self.compute_objective(cand, self.params)
+        return dist + self.compute_idle_wait(cand, self.params)
 
-    def insert(self, idx: int, customer: int) -> None:
+    def insert(self, idx: int, customer: int):
         """
         Insert the customer at position idx.
         """
         self.tour.insert(idx, customer)
 
-    def remove(self, customer: int) -> None:
+    def remove(self, customer: int):
         """
         Remove the customer from the current schedule.
         """
         self.tour.remove(customer)
+
+    def update(self, distance_cost=None, idle_wait_cost=None):
+        """
+        Update the current tour's total cost using the passed-in costs.
+
+        If no costs are passed-in, then re-compute the costs.
+        """
+        if distance_cost is None or idle_wait_cost is None:
+            self._distance_cost = self.compute_distance(self.tour, self.params)
+            self._idle_wait_cost = self.compute_idle_wait(self.tour, self.params)
+        else:
+            self._distance_cost = distance_cost
+            self._idle_wait_cost = idle_wait_cost
