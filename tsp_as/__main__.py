5a2b5b08c0d47a4f244c70a0f568db1c9c42c93e
l-lan <l.lan@vu.nl>
2022-11-08 15:58:37 +0100

    Separate tests/

--- tsp_as/__main__.py
+++ tsp_as/__main__.py
@@ -1,47 +1,8 @@
 import numpy as np
 import numpy.random as rnd
-from numpy.testing import assert_array_almost_equal, assert_array_equal
 
 from tsp_as.classes import Params
 from tsp_as.evaluations import heavy_traffic_optimal, heavy_traffic_pure, true_optimal
-from tsp_as.evaluations.heavy_traffic import compute_objective as ht_compute_objective
-from tsp_as.evaluations.heavy_traffic import compute_schedule as ht_compute_schedule
-from tsp_as.evaluations.true_optimal import compute_schedule as to_compute_schedule
-
-
-def test():
-    # Testing using Bharti's examples
-    omega_b = 0.8
-    means = 0.5 * np.ones(10)
-    SCVs = 0.5 * np.ones(10)
-    x = ht_compute_schedule(means, SCVs, omega_b)
-    cost = ht_compute_objective(means, SCVs, omega_b)
-
-    # Heavy traffic pure
-    assert_array_equal(
-        x, [0.625, 0.625, 0.625, 0.625, 0.625, 0.625, 0.625, 0.625, 0.625, 0.625]
-    )
-    assert np.isclose(cost, 2.0)
-
-    # True optimal
-    x, cost = to_compute_schedule(means, SCVs, omega_b)
-
-    assert_array_almost_equal(
-        x,
-        [
-            0.12324929,
-            0.29232458,
-            0.33362237,
-            0.34655884,
-            0.35010092,
-            0.34723355,
-            0.33850544,
-            0.32247991,
-            0.29126939,
-            0.21868121,
-        ],
-    )
-    assert np.isclose(cost, 1.1475057306039138)
 
 
 def main():
@@ -50,7 +11,6 @@ def main():
     omega_b = params.omega_b
     tour = np.arange(params.dimension)
 
-    test()
     costs = []
     for _ in range(100):
         cost_ = []
