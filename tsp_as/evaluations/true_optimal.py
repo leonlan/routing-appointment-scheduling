4d02636b613da5504530e4facd1b17ff4c2aa9ae
Leon Lan <30997278+leonlan@users.noreply.github.com>
2022-11-28 16:20:32 +0100

    Refactor true optimal (#25)
    * Separate tests/
    
    * Simplify create_Vn a bit
    
    * Simplify create_Vn further
    
    * Minor comments
    
    * Revert some TO simplifications
    
    * Add profiling to benchmark
    
    * Add all code tests (including expm_multiply)
    
    * Remove old experiments
    
    * Remove original tests
    
    * Finish refactoring

--- tsp_as/evaluations/true_optimal.py
+++ tsp_as/evaluations/true_optimal.py
@@ -1,10 +1,10 @@
 import math
 
 import numpy as np
-from scipy.linalg import inv  # matrix inversion
-from scipy.linalg.blas import dgemm, dgemv  # matrix multiplication
+from scipy.linalg import inv
+from scipy.linalg.blas import dgemm
 from scipy.optimize import LinearConstraint, minimize
-from scipy.sparse.linalg import expm  # matrix exponential
+from scipy.sparse.linalg import expm
 from scipy.stats import poisson
 
 from .tour2params import tour2params
@@ -12,7 +12,7 @@ from .tour2params import tour2params
 
 def phase_parameters(mean, SCV):
     """
-    Returns the initial distribution gamma and the transition rate
+    Returns the initial distribution alpha and the transition rate
     matrix T of the phase-fitted service times given the mean, SCV,
     and the elapsed service time u of the client in service.
     """
@@ -24,14 +24,14 @@ def phase_parameters(mean, SCV):
         # REVIEW where is the formula for this?
         mu = (K + 1 - p) / mean
 
-        # REVIEW where/what is gamma?
-        gamma_i = np.zeros((1, K + 1))
+        # REVIEW where/what is alpha?
+        alpha_i = np.zeros((1, K + 1))
         # REVIEW where/what is B-sf?
         B_sf = poisson.cdf(K - 1, mu) + (1 - p) * poisson.pmf(K, mu)
 
         # REVIEW where is this found?
-        gamma_i[0, :] = [poisson.pmf(z, mu) / B_sf for z in range(K + 1)]
-        gamma_i[0, K] *= 1 - p
+        alpha_i[0, :] = [poisson.pmf(z, mu) / B_sf for z in range(K + 1)]
+        alpha_i[0, K] *= 1 - p
 
         transition = -mu * np.eye(K + 1)
         transition += mu * np.diag(np.ones(K), k=1)  # one above diagonal
@@ -46,66 +46,83 @@ def phase_parameters(mean, SCV):
         B_sf = p * np.exp(-mu1) + (1 - p) * np.exp(-mu2)
         elt = p * np.exp(-mu1) / B_sf  # TODO find better name than element
 
-        gamma_i = np.array([elt, 1 - elt])
+        alpha_i = np.array([elt, 1 - elt])
         transition = np.diag([-mu1, -mu2])
 
-    return gamma_i, transition
+    return alpha_i, transition
 
 
-def create_Vn(gamma, T):
+def create_Vn(alphas, T):
     """
-    Creates the matrix Vn given the initial distributions `gamma` and the
+    Creates the Vn matrix given the initial distributions `alphas` and the
     corresponding transition matrices `T`.
+
+    alphas
+        List of initial distribution arrays
+    T
+        List of transition matrices
     """
-    # initialize Vn
     n = len(T)
     d = [T[i].shape[0] for i in range(n)]
-    dim_V = np.cumsum([0] + d)
-    Vn = np.zeros((dim_V[n], dim_V[n]))
+    dims = np.cumsum([0] + d)
 
-    # compute Vn recursively
+    Vn = np.zeros((dims[n], dims[n]))
+
     for i in range(1, n):
-        Vn[dim_V[i - 1] : dim_V[i], dim_V[i - 1] : dim_V[i]] = T[i - 1]
-        Vn[dim_V[i - 1] : dim_V[i], dim_V[i] : dim_V[i + 1]] = (
-            np.matrix(-T[i - 1] @ np.ones((d[i - 1], 1))) @ gamma[i]
-        )
+        l = dims[i - 1]
+        u = dims[i]
+        k = dims[i + 1]
 
-    Vn[dim_V[n - 1] : dim_V[n], dim_V[n - 1] : dim_V[n]] = T[n - 1]
+        Vn[l:u, l:u] = T[i - 1]
+        Vn[l:u, u:k] = -T[i - 1] @ np.ones((d[i - 1], 1)) @ alphas[i]
+
+    Vn[dims[n - 1] : dims[n], dims[n - 1] : dims[n]] = T[n - 1]
 
     return Vn
 
 
-def compute_objective(x, gamma, Vn, Vn_inv, omega_b):
+def compute_objective(x, alphas, Vn, omega_b):
     """
-    Compute the objective value of a schedule.
+    Compute the objective value of a schedule. See Theorem (1).
 
-    Theorem (1).
+    x
+        The interappointment times.
+    alphas
+        The alpha parameters of the phase-type distribution.
+    Vn
+        The recursively-defined matrix $V^{(n)}$.
+    omega_b
+        The weight associated with idle time.
+        # TODO This should become part of the params
+        # TODO Check if this is idle time or waiting time.
+
     """
-    n = len(gamma)
+    n = len(alphas)
+    omega = 0  # TODO this need to be added as parameter at some point
+    dims = np.cumsum([alphas[i].size for i in range(n)])
+    Vn_inv = inv(Vn)
 
-    Pi = gamma[0]
-
+    beta = alphas[0]
     cost = omega_b * np.sum(x)
-    dim_csum = np.cumsum([gamma[i].shape[1] for i in range(n)])
 
-    # cost of clients to be scheduled
     for i in range(n):
-        d = dim_csum[i]
-        exp_Vi = expm(Vn[:d, :d] * x[i])
-        expr = dgemv(
-            1,
-            dgemm(1, Pi, Vn_inv[:d, :d]),
-            np.sum(omega_b * np.eye(d) - exp_Vi, 1),
-        )
-        cost += expr[0]  # dgemv returns np.array
+        d = dims[i]
+        expVx = expm(Vn[:d, :d] * x[i])
 
-        # Stop creation of next matrices when all i are checked
-        if i == n - 1:
+        # The idle and waiting terms in the objective function can be decomposed
+        # in the following three terms, reducing several matrix computations.
+        term1 = dgemm(1, beta, Vn_inv[:d, :d])
+        term2 = (omega_b * np.eye(d) - (1 - omega) * expVx).sum(axis=1)
+        term3 = omega_b * x[i]
+
+        cost += np.dot(term1, term2)[0] + term3
+
+        if i == n - 1:  # stop
             break
 
-        P = dgemm(1, Pi, exp_Vi)
+        P = dgemm(1, beta, expVx)
         Fi = 1 - np.sum(P)
-        Pi = np.concatenate((P, gamma[i + 1] * Fi), axis=1)
+        beta = np.hstack((P, alphas[i + 1] * Fi))
 
     return cost
 
@@ -115,11 +132,10 @@ def compute_objective_(x, means, SCVs, omega_b):
     TODO This functions is used for HTM. Try to refactor with ``compute_objective``.
     """
     n = len(means)
-    gamma, T = zip(*[phase_parameters(means[i], SCVs[i]) for i in range(n)])
-    Vn = create_Vn(gamma, T)
-    Vn_inv = inv(Vn)
+    alpha, T = zip(*[phase_parameters(means[i], SCVs[i]) for i in range(n)])
+    Vn = create_Vn(alpha, T)
 
-    return compute_objective(x, gamma, Vn, Vn_inv, omega_b)
+    return compute_objective(x, alpha, Vn, omega_b)
 
 
 def compute_schedule(means, SCVs, omega_b, tol=None):
@@ -127,12 +143,11 @@ def compute_schedule(means, SCVs, omega_b, tol=None):
     Return the appointment times and the cost of the true optimal schedule.
     """
     n = len(means)
-    gamma, T = zip(*[phase_parameters(means[i], SCVs[i]) for i in range(n)])
-    Vn = create_Vn(gamma, T)
-    Vn_inv = inv(Vn)
+    alpha, T = zip(*[phase_parameters(means[i], SCVs[i]) for i in range(n)])
+    Vn = create_Vn(alpha, T)
 
     def cost_fun(x):
-        return compute_objective(x, gamma, Vn, Vn_inv, omega_b)
+        return compute_objective(x, alpha, Vn, omega_b)
 
     x_init = 1.5 * np.ones(n)
     lin_cons = LinearConstraint(np.eye(n), 0, np.inf)
