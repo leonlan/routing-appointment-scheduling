f1655fc6e4329ff7500c259829bb83b34d606443
l-lan <l.lan@vu.nl>
2023-04-12 09:44:12 +0200

    Use total SCV for increasing_scv function

--- tsp_as/increasing_scv.py
+++ tsp_as/increasing_scv.py
@@ -3,8 +3,9 @@ from tsp_as.classes import Solution
 
 def increasing_scv(data, seed, **kwargs):
     """
-    Creates a tour by ordering the clients in order of increasing SCV.
+    Creates a tour by ordering the clients in order of increasing SCVs from
+    the combined travel and service times.
     """
-    tour = data.service_scv.argsort().tolist()
-    tour.remove(0)  # Ignore the depot
+    tour = data.scvs.argsort().tolist()
+    tour.remove(0)  # ignore the depot
     return Solution(data, tour)
