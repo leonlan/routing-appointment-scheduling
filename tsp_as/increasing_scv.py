2223ff5bfd89b82d758cf736616a9f0d8372d102
l-lan <l.lan@vu.nl>
2023-04-13 09:57:48 +0200

    Fix increasing SCV
    SCV in order of service time is correct

--- tsp_as/increasing_scv.py
+++ tsp_as/increasing_scv.py
@@ -1,13 +1,25 @@
+from time import perf_counter
+
+from alns.Result import Result
+from alns.Statistics import Statistics
+
 from tsp_as.classes import Solution
 
 
-def increasing_scv(data, seed, **kwargs):
+def increasing_scv(seed, data, **kwargs):
     """
-    Creates a tour by ordering the clients in order of increasing SCVs from
-    the combined travel and service times.
+    Creates a tour in increasing order of service time SCVs.
     """
-    tour = data.scvs.argsort().tolist()
+    start = perf_counter()
+
+    tour = data.service_scv.argsort().tolist()
     tour.remove(0)  # ignore the depot
     solution = Solution(data, tour)
 
-    return solution
+    # This little hack allows us to use the same interface for ALNS-based
+    # heuristics and the SCV heuristic.
+    stats = Statistics()
+    stats.collect_objective(solution.cost)
+    stats.collect_runtime(perf_counter() - start)
+
+    return Result(solution, stats)
