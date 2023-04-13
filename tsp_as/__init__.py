fc4504bb7602fb6b6b835c41d6041ed4848dc5f0
l-lan <l.lan@vu.nl>
2023-04-13 22:06:14 +0200

    Increasing variance

--- tsp_as/__init__.py
+++ tsp_as/__init__.py
@@ -1,4 +1,5 @@
 from .increasing_scv import increasing_scv
+from .increasing_variance import increasing_variance
 from .solve_alns import solve_alns
 from .solve_modified_tsp import solve_modified_tsp
 from .solve_tsp import solve_tsp
