7bd9008239c0c86c63d5e36212a413f9344c73b9
l-lan <l.lan@vu.nl>
2023-05-09 16:10:49 +0200

    Use LKH (elkai) to solve TSP

--- tsp_as/solve_tsp.py
+++ tsp_as/solve_tsp.py
@@ -1,17 +1,22 @@
-from copy import deepcopy
+from time import perf_counter
 
-from .solve_alns import solve_alns
+import elkai
+from alns.Result import Result
+from alns.Statistics import Statistics
+
+from tsp_as.classes import Solution
 
 
-def solve_tsp(seed, data, **kwargs):
+def solve_tsp(seed, data, max_iterations=1000, **kwargs):
     """
     Solves the TSP without appointment scheduling.
     """
-    tsp_data = deepcopy(data)
+    start = perf_counter()
+    tour = elkai.solve_float_matrix(data.distances, runs=max_iterations)
+    tour.remove(0)  # remove depot
 
-    # Set omegas to focus purely on routing
-    tsp_data.omega_idle = 0
-    tsp_data.omega_wait = 0
-    tsp_data.omega_travel = 1
+    stats = Statistics()
+    stats.collect_objective(0)
+    stats.collect_runtime(perf_counter() - start)
 
-    return solve_alns(seed, data=tsp_data, **kwargs)
+    return Result(Solution(data, tour), stats)
