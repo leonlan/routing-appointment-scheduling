a12e501f8900bc9b533f43603b2839e74745fe05
l-lan <l.lan@vu.nl>
2023-05-09 11:06:01 +0200

    Add time-based RRT for ALNS

--- tsp_as/solve_alns.py
+++ tsp_as/solve_alns.py
@@ -1,7 +1,9 @@
+import time
+
 import numpy as np
 import numpy.random as rnd
 from alns import ALNS
-from alns.accept import HillClimbing, RecordToRecordTravel
+from alns.accept import RecordToRecordTravel
 from alns.select import RouletteWheel
 from alns.stop import MaxIterations, MaxRuntime
 
@@ -34,14 +36,99 @@ def solve_alns(
 
     select = RouletteWheel([5, 2, 1, 0.5], 0.5, len(D_OPS), len(R_OPS))
 
+    start_threshold = 0.05
+    end_threshold = 0
+
     if max_runtime is not None:
         stop = MaxRuntime(max_runtime)
-        accept = HillClimbing()
+        accept = TimeBasedRecordToRecordTravel(
+            start_threshold, end_threshold, max_runtime
+        )
     else:
         assert max_iterations is not None
         stop = MaxIterations(max_iterations)
-        accept = RecordToRecordTravel.autofit(init.objective(), 0.02, 0, max_iterations)
+        accept = RecordToRecordTravel.autofit(
+            init.objective(), start_threshold, end_threshold, max_iterations
+        )
 
-    res = alns.iterate(init, select, accept, stop, **kwargs)
+    return alns.iterate(init, select, accept, stop, **kwargs)
 
-    return res
+
+def time_based_value(
+    start_value: float,
+    end_value: float,
+    max_runtime: float,
+    start_time: float,
+    method: str,
+):
+    if max_runtime < 0:
+        raise ValueError("max_runtime must be >= 0")
+
+    if start_value < end_value:
+        raise ValueError("start_value must be >= end_value")
+
+    if end_value < 0 and method == "exponential":
+        raise ValueError("end_value must be > 0 for exponential method")
+
+    if max_runtime == 0:
+        return end_value
+
+    # pct_elapsed_time=0 should give the start temperature,
+    # pct_elapsed_time=1 should give the end temperature.
+    pct_elapsed_time = (time.perf_counter() - start_time) / max_runtime
+    pct_elapsed_time = min(pct_elapsed_time, 1)
+
+    if method == "linear":
+        delta = start_value - end_value
+        return start_value - delta * pct_elapsed_time
+    elif method == "exponential":
+        fraction = end_value / start_value
+        return start_value * fraction**pct_elapsed_time
+    else:
+        raise ValueError(f"Method {method} not known.")
+
+
+class TimeBasedRecordToRecordTravel:
+    def __init__(
+        self,
+        start_threshold: float,
+        end_threshold: float,
+        max_runtime: float,
+        method: str = "linear",
+        cmp_best: bool = True,
+    ):
+        self._start_threshold = start_threshold
+        self._end_threshold = end_threshold
+        self._max_runtime = max_runtime
+        self._method = method
+        self._cmp_best = cmp_best
+
+        self._delta_threshold = start_threshold - end_threshold
+        self._start_time = time.perf_counter()
+
+    @property
+    def start_threshold(self) -> float:
+        return self._start_threshold
+
+    @property
+    def end_threshold(self) -> float:
+        return self._end_threshold
+
+    @property
+    def max_runtime(self) -> float:
+        return self._max_runtime
+
+    @property
+    def method(self) -> str:
+        return self._method
+
+    def __call__(self, rnd, best, curr, cand):
+        threshold = time_based_value(
+            self.start_threshold,
+            self.end_threshold,
+            self.max_runtime,
+            self._start_time,
+            self.method,
+        )
+        baseline = best if self._cmp_best else curr
+        return cand.objective() - baseline.objective() <= threshold
