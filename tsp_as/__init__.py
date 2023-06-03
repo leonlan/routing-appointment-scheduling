780218564c66eb6c68efb57571896ea0cfa7c6a0
l-lan <l.lan@vu.nl>
2023-06-03 13:41:01 +0200

    Cost evaluator to simple heuristics

--- tsp_as/__init__.py
+++ tsp_as/__init__.py
@@ -1,5 +1,4 @@
 from .full_enumeration import full_enumeration
-from .increasing_scv import increasing_scv
 from .increasing_variance import increasing_variance
 from .solve_alns import solve_alns
 from .solve_modified_tsp import solve_modified_tsp
