0664a0ce64b4db14ecef3effc9631181783bc996
l-lan <l.lan@vu.nl>
2023-06-04 16:02:22 +0200

    Finish heavy traffic

--- tsp_as/appointment/heavy_traffic.py
+++ tsp_as/appointment/heavy_traffic.py
@@ -6,8 +6,6 @@ from .utils import get_leg_data, get_vars
 def compute_schedule(tour, data, cost_evaluator):
     """
     Computes the schedule using heavy traffic approximation.
-
-    Eq. (2) in draft.
     """
     means, _, _ = get_leg_data(tour, data)
     S = _compute_weighted_mean_variance(tour, data)
@@ -21,14 +19,11 @@ def compute_schedule(tour, data, cost_evaluator):
 def compute_objective(tour, schedule, data) -> tuple[list[float], list[float]]:
     """
     Computes the objective value using heavy traffic approximation.
-    See (3) in draft.
     """
+    means, _, _ = get_leg_data(tour, data)
     S = _compute_weighted_mean_variance(tour, data)
-    frm = [0] + tour[:-1]
-    to = tour  # TODO visits2tour
-    travel_times = data.means[frm, to]
 
-    idle_times = schedule - travel_times
+    idle_times = schedule - means
     wait_times = S / (2 * idle_times)
 
     return idle_times, wait_times
@@ -39,7 +34,7 @@ def _compute_weighted_mean_variance(tour, data):
     This function mimics the notation in the paper for easy checking.
     There's a much more compact way to do this, see the this commit:
 
-    https://github.com/leonlan/tsp-as/blob/7bd9008/tsp_as/appointment/ heavy_traffic.py#L30
+    https://github.com/leonlan/tsp-as/blob/7bd9008/tsp_as/appointment/heavy_traffic.py#L30
     """
     n = len(tour)
     BETA = 0.5  # TODO I'm not sure what this paramater is.
