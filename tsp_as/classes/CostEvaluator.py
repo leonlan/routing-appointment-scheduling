1c0f8ceff745ee247c5e67e978bd9b07beaa94b9
l-lan <l.lan@vu.nl>
2023-06-04 09:48:49 +0200

    Rename penalties to weight in CostEvaluator

--- tsp_as/classes/CostEvaluator.py
+++ tsp_as/classes/CostEvaluator.py
@@ -5,30 +5,30 @@ import numpy as np
 
 class CostEvaluator:
     def __init__(
-        self, travel_penalty: float, idle_penalty: float, wait_penalties: list[float]
+        self, travel_weight: float, idle_weight: float, wait_weights: list[float]
     ):
         """
         Parameters
         ----------
-        travel_penalty
-            Penalty for travel time.
-        idle_penalty
-            Penalty for idle time.
-        wait_penalties
-            List of wait penalties for each client.
+        travel_weight
+            Weight for travel time.
+        idle_weight
+            Weight for idle time.
+        wait_weights
+            List of wait weights for each client.
         """
-        self.travel_penalty = travel_penalty
-        self.idle_penalty = idle_penalty
-        self.wait_penalties = np.array(wait_penalties)
+        self.travel_weight = travel_weight
+        self.idle_weight = idle_weight
+        self.wait_weights = np.array(wait_weights)
 
     def __call__(self, solution: Solution) -> float:  # noqa: F821
         """
         Returns the cost of a solution.
         """
-        travel_costs = self.travel_penalty * solution.distance
-        idle_costs = np.dot(self.idle_penalty, solution.idle_times).sum()
+        travel_costs = self.travel_weight * solution.distance
+        idle_costs = np.dot(self.idle_weight, solution.idle_times).sum()
 
-        wait_penalties = self.wait_penalties[solution.tour]
-        wait_costs = np.dot(wait_penalties, solution.waiting_times)
+        wait_weights = self.wait_weights[solution.tour]
+        wait_costs = np.dot(wait_weights, solution.waiting_times)
 
         return travel_costs + idle_costs + wait_costs
